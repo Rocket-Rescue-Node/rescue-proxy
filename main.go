5d639f57dc06c052501528d8ecb6a5537fd0bc44
Jacob Shufro <jacob@shuf.ro>
2023-01-08 17:09:49 -0500

    Add TLS cert/key file support for grpc

--- main.go
+++ main.go
@@ -34,6 +34,8 @@ type config struct {
 	AdminListenAddr    string
 	GRPCListenAddr     string
 	GRPCBeaconAddr     string
+	GRPCTLSCertFile    string
+	GRPCTLSKeyFile     string
 	RocketStorageAddr  string
 	CredentialSecret   string
 	AuthValidityWindow time.Duration
@@ -62,6 +64,8 @@ func initFlags() (config config) {
 	apiAddrURLFlag := flag.String("api-addr", "0.0.0.0:8080", "Address on which to reply to gRPC API requests")
 	grpcAddrFlag := flag.String("grpc-addr", "", "Address on which to reply to gRPC requests")
 	grpcBeaconAddrFlag := flag.String("grpc-beacon-addr", "", "Address to the beacon node to proxy for gRPC, eg, localhost:4000")
+	grpcTLSCertFileFlag := flag.String("grpc-tls-cert-file", "", "Optional TLS Certificate for the gRPC host")
+	grpcTLSKeyFileFlag := flag.String("grpc-tls-key-file", "", "Optional TLS Key for the gRPC host")
 	rocketStorageAddrFlag := flag.String("rocketstorage-addr", "0x1d8f8f00cfa6758d7bE78336684788Fb0ee0Fa46", "Address of the Rocket Storage contract. Defaults to mainnet")
 	debug := flag.Bool("debug", false, "Whether to enable verbose logging")
 	credentialSecretFlag := flag.String("hmac-secret", "test-secret", "The secret to use for HMAC")
@@ -106,7 +110,6 @@ func initFlags() (config config) {
 	}
 	config.ExecutionURL = base
 
-	// TODO: Support grpc:// as a protocol type
 	if config.BeaconURL.Scheme != "http" && config.BeaconURL.Scheme != "https" {
 		fmt.Fprintf(os.Stderr, "Invalid -bn-url: %s\nOnly http and https Beacon Nodes are supported right now.\n", *bnURLFlag)
 		os.Exit(1)
@@ -153,6 +156,16 @@ func initFlags() (config config) {
 	config.AuthValidityWindow, err = time.ParseDuration(*authValidityWindowFlag)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Invalid -auth-valid-for:\n%v\n", err)
+		os.Exit(1)
+		return
+	}
+
+	config.GRPCTLSCertFile = *grpcTLSCertFileFlag
+	config.GRPCTLSKeyFile = *grpcTLSKeyFileFlag
+	if (config.GRPCTLSCertFile == "" && config.GRPCTLSKeyFile != "") ||
+		(config.GRPCTLSCertFile != "" && config.GRPCTLSKeyFile == "") {
+
+		fmt.Fprintf(os.Stderr, "If either --grpc-tls-key-file or --grpc-tls-cert-file is set, both must be set")
 		os.Exit(1)
 		return
 	}
@@ -299,6 +312,9 @@ func main() {
 			Logger:             logger,
 			AuthValidityWindow: config.AuthValidityWindow,
 		}
+
+		grpcRouter.TLS.CertFile = config.GRPCTLSCertFile
+		grpcRouter.TLS.KeyFile = config.GRPCTLSKeyFile
 
 		err := grpcRouter.Init(config.GRPCListenAddr, config.GRPCBeaconAddr)
 		if err != nil {
